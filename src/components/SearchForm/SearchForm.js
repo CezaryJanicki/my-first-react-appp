3c2a2f0cdd189fa7877198560b8e42769442fce5
CezaryJanicki <cezary.janicki@gmail.com>
2023-06-09 09:27:09 +0200

    Add SearchForm ClassNames

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -3,8 +3,8 @@ import styles from './SearchForm.module.scss';
 const SearchForm = () => {
     return (
         <form className={styles.searchForm}>
-            <input type="text" class="input"/>
-            <button class="button">Search</button>
+            <input type="text" className={styles.input}/>
+            <button className={styles.button}>Search</button>
         </form>
     );
   };
