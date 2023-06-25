fae4f48d2b324009d410354fccd917426ceeb538
CezaryJanicki <cezary.janicki@gmail.com>
2023-06-25 18:30:43 +0200

    Add Home, Favourites and About components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import Hero from './components/Hero/Hero.js';
-import SearchForm from './components/SearchForm/SearchForm.js';
-import List from './components/List/List.js';
 import Container from './components/Container/Container.js'
 import NavBar from './components/NavBar/NavBar.js';
+import Home from './components/Home/Home.js'
+import Favourites from './components/Favourites/Favourites.js'
+import About from './components/About/About.js'
 
 const App = () => {
   return (
     <main>
     <NavBar />
     <Container>
-      <Hero />
-      <SearchForm />
-      <List />
+      <Home />
+      <Favourites />
+      <About />
     </Container>
     </main>
   );
---
+++ src/components/Favourites/Favourites.js
@@ -0,0 +1,16 @@
+import styles from './Favourites.module.scss';
+
+const Favorites = () => {
+  return (
+    <div>
+      <div className={styles.hero}>
+        <h1 className={styles.title}>Favorites</h1>
+        <p className={styles.subtitle}>
+          Lorem ipsum
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Favorites;
