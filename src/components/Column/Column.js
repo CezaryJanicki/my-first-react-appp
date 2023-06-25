485e8c627967fbdeb51538e881df1d329665a15e
CezaryJanicki <cezary.janicki@gmail.com>
2023-06-25 15:05:14 +0200

    Add Redux and access to storage

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,12 +1,15 @@
 import styles from './Column.module.scss'
 import Card from '../Card/Card';
 import CardForm from '../CardForm/CardForm';
+import { useSelector } from 'react-redux';
 
 const Column = props => {
+
+    const cards =  useSelector(state => state.cards.filter(card => card.columnId === props.id));
     return (
         <article className={styles.column}>
             <h3 className={styles.title}><span className={styles.icon + ' fa fa-' + props.icon} />{props.title}</h3>
-            <ul className={styles.cards}> {props.cards.map(card => <Card key={card.id} title={card.title} />)}</ul>
+            <ul className={styles.cards}> {cards.map(card => <Card key={card.id} title={card.title} />)}</ul>
            <CardForm columnId={props.id} action={props.action}/>
         </article>
     );
--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import ReactDOM from 'react-dom';
+import React from 'react';
 import App from './App';
 import './styles/normalize.scss';
 import './styles/global.scss';
 import 'font-awesome/css/font-awesome.min.css';
 
+import { Provider } from 'react-redux';
+import store from './redux/store'
+
 ReactDOM.render(
-  <App />,
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
   document.querySelector('#root')
 );
